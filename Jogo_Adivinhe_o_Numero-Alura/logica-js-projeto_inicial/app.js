401a12dc775e7d475b1202c162f09825f5fb38d0
AnaShabests <ana.shabests@triggolabs.com>
2024-10-24 18:46:13 -0300

    Alteração do número máximo

--- Jogo_Adivinhe_o_Numero-Alura/logica-js-projeto_inicial/app.js
+++ Jogo_Adivinhe_o_Numero-Alura/logica-js-projeto_inicial/app.js
@@ -1,9 +1,9 @@
 alert("Bem vindo ao jogo do número secreto!!"); //alert serve para exibir uma mensagem em uma caixinha assim que o usuário acessar a página
 //não é sempre que se utiliza o ;
-
+let numeroMaximo = 100;
 //Math.random() gera um número aleatório entre 0(incluso) e 1 (não incluso)
 //let numeroSecreto = parseInt(Math.random() * 10 + 1); //parseInt serve para transformar o número em inteiro; *10 pq queremos um número entre 0 e 9 (pois o 1 n é incluso); +1 pq queremos um número entre 1 e 10
-let numeroSecreto = parseInt(Math.random() * 100 + 1); //parseInt serve para transformar o número em inteiro; *100 pq queremos um número entre 0 e 99 (pois o 1 n é incluso); +1 pq queremos um número entre 1 e 100
+let numeroSecreto = parseInt(Math.random() * numeroMaximo) + 1; //parseInt serve para transformar o número em inteiro; *100 pq queremos um número entre 0 e 99 (pois o 1 n é incluso); +1 pq queremos um número entre 1 e 100
 
 //let comando para armazenar na memória
 //let numeroSecreto = 9;
@@ -13,7 +13,7 @@ let chute; //prompt serve para exibir uma caixinha para interagir com o usuário
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt("Digite um número entre 1 a 100");
+    chute = prompt(`Digite um número entre 1 a ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
         console.log("Parabéns, você acertou :)");
